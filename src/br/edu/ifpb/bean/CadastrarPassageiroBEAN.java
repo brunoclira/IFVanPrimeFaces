bcfcc8e8a4c9f4dc556626251abe467e09202c3e
Bruno Lira <brunolyr4@gmail.com>
2016-03-11 19:13:22 -0300

    update

--- src/br/edu/ifpb/bean/CadastrarPassageiroBEAN.java
+++ src/br/edu/ifpb/bean/CadastrarPassageiroBEAN.java
@@ -1,16 +1,52 @@
 package br.edu.ifpb.bean;
 
+import javax.annotation.PostConstruct;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.context.FacesContext;
+
+import java.util.HashMap;
+import java.util.Map;
+ 
  
 @ManagedBean(name = "CadastrarPassageiroBEAN")
 public class CadastrarPassageiroBEAN {
      
+	private Map<String,Map<String,String>> data = new HashMap<String, Map<String,String>>();
     private String firstname;
     private String lastname;
- 
-    public String getFirstname() {
+    private String cpf;
+    private String sexo;
+    private Map<String,String> sexos;
+    
+    @PostConstruct
+    public void init() {
+        sexos  = new HashMap<String, String>();
+        sexos.put("Feminino", "Feminino");
+        sexos.put("Masculino", "Masculino");
+    }
+    
+    public Map<String, String> getSexos() {
+        return sexos;
+    }
+    
+    public String getCpf() {
+		return cpf;
+	}
+
+	public void setCpf(String cpf) {
+		this.cpf = cpf;
+	}
+
+	public String getSexo() {
+		return sexo;
+	}
+
+	public void setSexo(String sexo) {
+		this.sexo = sexo;
+	}
+
+	public String getFirstname() {
         return firstname;
     }
  
@@ -25,9 +61,20 @@ public class CadastrarPassageiroBEAN {
     public void setLastname(String lastname) {
         this.lastname = lastname;
     }
+    
+    public void displayLocation() {
+        FacesMessage msg;
+        if(sexo != null)
+            msg = new FacesMessage("Selected" + sexo);
+        else
+            msg = new FacesMessage(FacesMessage.SEVERITY_ERROR, "Invalid", "Sexo is not selected."); 
+             
+        FacesContext.getCurrentInstance().addMessage(null, msg);  
+    }
  
     public void save() {
         FacesContext.getCurrentInstance().addMessage(null,
                 new FacesMessage("Welcome " + firstname + " " + lastname));
     }
+    
 }
