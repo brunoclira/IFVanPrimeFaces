7fd91bf44c1b742c6ff472ea3df0564a6e3a58a6
leonardo <leosdesousa@gmail.com>
2016-03-12 19:34:37 -0300

    Retirando bug do meu Git

--- src/br/edu/ifpb/DAO/ComprarPassagemDAO.java
+++ src/br/edu/ifpb/DAO/ComprarPassagemDAO.java
@@ -1,5 +1,26 @@
+
 package br.edu.ifpb.DAO;
 
-public class ComprarPassagemDAO {
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 
+import br.edu.ifpb.domain.Van;
+
+public class ComprarPassagemDAO {
+	
+public void salvar(Van v) throws ClassNotFoundException, SQLException{
+		
+		StringBuilder sql = new StringBuilder();
+		
+		sql.append("UPDATE van SET cpf_id='" + v.getCpf() + "',estado='busy',tipo_passagem='" + v.getTipo() + "'"
+				+ ",valor='" + v.getValor() + "'WHERE cadeira=" + v.getCadeira() + ";");
+		
+		ConnectionFactory.openConnection();
+		Connection con = ConnectionFactory.getConnection();
+		
+		PreparedStatement preparador = con.prepareStatement(sql.toString());
+		
+		preparador.executeUpdate();
+	}
 }
--- src/br/edu/ifpb/bean/ComprarPassagemBEAN.java
+++ src/br/edu/ifpb/bean/ComprarPassagemBEAN.java
@@ -1,31 +1,39 @@
 package br.edu.ifpb.bean;
 
+import java.sql.SQLException;
 import java.util.HashMap;
 import java.util.Map;
 
 import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
 
-import br.edu.ifpb.domain.Passageiro;
+import org.primefaces.context.RequestContext;
 
+import br.edu.ifpb.DAO.ComprarPassagemDAO;
+import br.edu.ifpb.domain.Van;
+import br.edu.ifpb.util.JSFUtil;
+
+@ManagedBean(name = "ComprarPassagemBEAN")
 public class ComprarPassagemBEAN {
 	
 	private Van van = new Van();
 
 	private Map<String,String> poltronas,tipo_passagens;
-
+	
     @PostConstruct
     public void init() {
+    	
         poltronas  = new HashMap<String, String>();
-        poltronas.put("Cadeira 1", "Cadeira 1");
-        poltronas.put("Cadeira 2", "Cadeira 2");
-        poltronas.put("Cadeira 3", "Cadeira 3");
-        poltronas.put("Cadeira 4", "Cadeira 4");
-        poltronas.put("Cadeira 5", "Cadeira 5");
-        poltronas.put("Cadeira 6", "Cadeira 6");
-        poltronas.put("Cadeira 7", "Cadeira 7");
-        poltronas.put("Cadeira 8", "Cadeira 8");
-        poltronas.put("Cadeira 9", "Cadeira 9");
-        poltronas.put("Cadeira 10", "Cadeira 10");
+        poltronas.put("Cadeira 1", "1");
+        poltronas.put("Cadeira 2", "2");
+        poltronas.put("Cadeira 3", "3");
+        poltronas.put("Cadeira 4", "4");
+        poltronas.put("Cadeira 5", "5");
+        poltronas.put("Cadeira 6", "6");
+        poltronas.put("Cadeira 7", "7");
+        poltronas.put("Cadeira 8", "8");
+        poltronas.put("Cadeira 9", "9");
+        poltronas.put("Cadeira 10", "10");
         
         tipo_passagens = new HashMap<String, String>();
         tipo_passagens.put("Estudante", "Estudante");
@@ -40,10 +48,6 @@ public class ComprarPassagemBEAN {
     public void setVan(Van van) {
 		this.van = van;
 	}
-    
-    public Map<String, String> getSexos() {
-        return sexos;
-    }
     
     public Map<String, String> getPoltronas() {
 		return poltronas;
@@ -60,5 +64,23 @@ public class ComprarPassagemBEAN {
     public void setTipo_passagens(Map<String, String> tipo_passagens) {
 		this.tipo_passagens = tipo_passagens;
 	}
+    
+    public void save() {
+    	try {
+    		ComprarPassagemDAO dao = new ComprarPassagemDAO();
+			dao.salvar(van);
+			
+			JSFUtil.messagemSucesso(van.getCpf() + " comprado com Sucesso");
+			
+		} catch (SQLException | ClassNotFoundException e) {
+			e.printStackTrace();
+			
+			JSFUtil.messagemError(e.getMessage());
+		} 
+    }
+    
+    public void reset() {
+        RequestContext.getCurrentInstance().reset("form:panel");
+    }
 
 }
