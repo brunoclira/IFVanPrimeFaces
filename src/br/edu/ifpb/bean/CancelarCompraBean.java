0bc3fae074c053e84e4550d1d6f1f1fa4c4028f7
leonardo <leosdesousa@gmail.com>
2016-03-12 18:11:53 -0300

    Update- Cancelando em funcionamento.

--- src/br/edu/ifpb/bean/CancelarCompraBean.java
+++ src/br/edu/ifpb/bean/CancelarCompraBean.java
@@ -6,15 +6,64 @@ import javax.faces.bean.ManagedBean;
 import java.util.ArrayList;
 import java.util.List;
 
+import br.edu.ifpb.DAO.CadastrarPassageiroDAO;
 import br.edu.ifpb.DAO.CadeiraVanDAO;
 import br.edu.ifpb.domain.Passageiro;
 import br.edu.ifpb.domain.Van;
-
-@ManagedBean(name = "CancelarCompraBean")
+@ManagedBean
 public class CancelarCompraBean {
 		private List<Van> lugares;
+		private Van van;
+		private String cpfRemover;
 
+		public String getCpfRemover() {
+			return cpfRemover;
+		}
 
+		public void setCpfRemover(String cpfRemover) {
+			this.cpfRemover = cpfRemover;
+		}
+
+		public List<Van> getLugares() {
+	
+			Van van = new Van();
+			CadeiraVanDAO vanDao = new CadeiraVanDAO();
+
+			try {
+				lugares = (ArrayList<Van>) vanDao.getLugares();
+			} catch (ClassNotFoundException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+
+			return lugares;
+		}
+
+		public void setLugares(List<Van> lugares) {
+			this.lugares = lugares;
+		}
+		public CancelarCompraBean(){
+			this.van=new Van();
+		}
+
+		public Van getVan() {
+			return van;
+		}
+
+		public void setVan(Van van) {
+			this.van = van;
+		}
+		
+public void cancelar(){
+	CadeiraVanDAO vanDao=new CadeiraVanDAO();
+	try {
+		vanDao.cancelarPassagem(this.cpfRemover);
+	} catch (ClassNotFoundException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	
+}
 
 
 
