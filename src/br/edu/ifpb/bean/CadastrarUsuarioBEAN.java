1f4fa80c2c133377d43edd46f83c53772c9bee47
Bruno Lira <brunolyr4@gmail.com>
2016-03-24 23:12:09 -0300

    update

--- src/br/edu/ifpb/bean/CadastrarUsuarioBEAN.java
+++ src/br/edu/ifpb/bean/CadastrarUsuarioBEAN.java
@@ -8,6 +8,7 @@ import javax.faces.context.FacesContext;
 
 import br.edu.ifpb.DAO.UsuarioDAO;
 import br.edu.ifpb.domain.Usuario;
+import br.edu.ifpb.util.JSFUtil;
 
 @ManagedBean(name = "CadastrarUsuarioBEAN")
 @RequestScoped
@@ -28,11 +29,11 @@ public class CadastrarUsuarioBEAN {
 	}
 
 	public void cadastrar() throws IOException {
-		System.out.println("chegou");
 		UsuarioDAO usuarioDao = new UsuarioDAO();
 
 		try {
 			usuarioDao.cadastrarUsuario(usuario);
+			JSFUtil.messagemSucesso(usuario.getNome() + " Cadastrado com Sucesso");
 			FacesContext.getCurrentInstance().getExternalContext().redirect("index.xhtml");
 		} catch (ClassNotFoundException e) {
 
