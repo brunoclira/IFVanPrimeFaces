fa0fbd90e036ae900cca9bc53f72e2976a0b413f
leonardo <leosdesousa@gmail.com>
2016-03-29 09:18:08 -0300

    Teste...

--- src/br/edu/ifpb/bean/CancelarCompraBean.java
+++ src/br/edu/ifpb/bean/CancelarCompraBean.java
@@ -1,6 +1,5 @@
 package br.edu.ifpb.bean;
 
-
 import javax.faces.bean.ManagedBean;
 
 import com.google.gson.Gson;
@@ -15,65 +14,62 @@ import java.util.List;
 import br.edu.ifpb.domain.Passageiro;
 import br.edu.ifpb.domain.Van;
 import br.edu.ifpb.util.JSFUtil;
+
 @ManagedBean
 public class CancelarCompraBean {
-		private List<Van> lugares;
-		private Van van;
-		private String cpfRemover;
+	private List<Van> lugares;
+	private Van van;
+	private String cpfRemover;
 
-		public String getCpfRemover() {
-			return cpfRemover;
-		}
+	public String getCpfRemover() {
+		return cpfRemover;
+	}
 
-		public void setCpfRemover(String cpfRemover) {
-			this.cpfRemover = cpfRemover;
-		}
+	public void setCpfRemover(String cpfRemover) {
+		this.cpfRemover = cpfRemover;
+	}
+
+	public List<Van> getLugares() {
+
+		Van van = new Van();
+		Client cliente = new Client();
+
+		WebResource wr = cliente.resource("http://localhost:8080/IFVanServico/services/cadeira/listar/");
+		String json = wr.get(String.class);
+		Gson gson = new Gson();
+
+		return gson.fromJson(json, new TypeToken<List<Van>>() {
+		}.getType());
+	}
+
+	public void setLugares(List<Van> lugares) {
+		this.lugares = lugares;
+	}
+
+	public CancelarCompraBean() {
+		this.van = new Van();
+	}
+
+	public Van getVan() {
+		return van;
+	}
+
+	public void setVan(Van van) {
+		this.van = van;
+	}
+
+	public void cancelar() {
+		Gson gson = new Gson();
+		System.out.println();
+
+		Client client = Client.create();
+
+		WebResource webResource = client.resource("http://localhost:8080/IFVanServico/services/cancelar");
 		
-		public List<Van> getLugares() {
-	
-			Van van = new Van();
-			Client cliente=new Client();
-			
-			WebResource wr=cliente.resource("http://localhost:8080/IFVanServico/services/cadeira/listar/");
-			String json=wr.get(String.class);
-			Gson gson=new Gson();
-			
-			return gson.fromJson(json, new TypeToken<List<Van>>(){}.getType());
-		}
+		String input = cpfRemover;
 
-		public void setLugares(List<Van> lugares) {
-			this.lugares = lugares;
-		}
-		public CancelarCompraBean(){
-			this.van=new Van();
-		}
+		ClientResponse response = webResource.type("application/json").post(ClientResponse.class, input);
 
-		public Van getVan() {
-			return van;
-		}
-
-		public void setVan(Van van) {
-			this.van = van;
-		}
-
-public void cancelar(){
-	Gson gson=new Gson();
-	System.out.println();
-
-	
-	Client client = Client.create();
-
-	WebResource webResource = client
-	   .resource("http://localhost:8080/IFVanServico/services/cancelar");
-	String input =cpfRemover;
-
-	ClientResponse response = webResource.type("application/json")
-	   .post(ClientResponse.class, input);
-	
-}
-
-
+	}
 
 }
-
-
